13dbb62ae0b98e1378ebef359fc2acfd93af6390
Andrey Dementev <andreydem42@gmail.com>
2021-09-26 11:46:16 +0300

    add comments

--- main/computer.py
+++ main/computer.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
 
+# Норма калорий для мужчины
 def norm_man(weight, height, birthdate):
     now = datetime.now()
     return int(66.5 + 13.75 * weight + 5.003 * height - 6.755 * (
                 now.year - birthdate.year - (1 if now.month < birthdate.month else 0)))
 
 
+# Норма калорий для женщины
 def norm_woman(weight, height, birthdate):
     now = datetime.now()
     return int(55.1 + 9.563 * weight + 1.85 * height - 4.676 * (
