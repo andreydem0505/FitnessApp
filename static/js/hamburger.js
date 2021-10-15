3a0c93dda95cb152044c531ae7c4d46fcae942af
Andrey Dementev <andreydem42@gmail.com>
2021-10-15 21:09:35 +0300

    Fixing

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,14 +1,28 @@
-let hamburger = document.getElementById("hamburger");
 let menu = document.getElementById("menu");
 let open = false;
-hamburger.onclick = () => {
+document.addEventListener("click", function () {
+    if (open) {
+        closeMenu();
+        open = false;
+    }
+}, false);
+document.getElementById("hamburger").addEventListener("click", function (ev) {
     open = !open;
     if (!open) {
-        menu.style.animationName = "disappear";
-        menu.onanimationend = () => menu.style.display = "none";
+       closeMenu();
     } else {
-        menu.style.display = "block";
-        menu.style.animationName = "appear";
-        menu.onanimationend = null;
+        openMenu();
     }
-};
+    ev.stopPropagation();
+}, false);
+
+function closeMenu() {
+    menu.style.animationName = "disappear";
+    menu.onanimationend = () => menu.style.display = "none";
+}
+
+function openMenu() {
+    menu.style.display = "block";
+    menu.style.animationName = "appear";
+    menu.onanimationend = null;
+}
